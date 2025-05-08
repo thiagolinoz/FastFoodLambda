7f1d64d329fa103d692d02d06b662a5760a0a93a
Alex Carreira <alex@cgtecnologia.com.br>
2025-05-07 22:25:09 -0300

    Finalizado o cadastro de cliente;

--- src/main/java/br/com/fiap/postechfastfood/infrastructure/persistence/jpa/repositories/PessoaRepository.java
+++ src/main/java/br/com/fiap/postechfastfood/infrastructure/persistence/jpa/repositories/PessoaRepository.java
@@ -6,6 +6,7 @@ import br.com.fiap.postechfastfood.infrastructure.persistence.jpa.entities.Pesso
 import br.com.fiap.postechfastfood.infrastructure.persistence.jpa.mappers.PessoaMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Repository
 public class PessoaRepository implements PessoaRepositoryPort {
 
     @PersistenceContext
--- src/main/java/br/com/fiap/postechfastfood/infrastructure/web/api/controllers/PessoaController.java
+++ src/main/java/br/com/fiap/postechfastfood/infrastructure/web/api/controllers/PessoaController.java
@@ -1,11 +1,38 @@
 package br.com.fiap.postechfastfood.infrastructure.web.api.controllers;
 
+import br.com.fiap.postechfastfood.domain.services.PessoaService;
+import br.com.fiap.postechfastfood.infrastructure.web.api.dtos.PessoaRequestDto;
+import br.com.fiap.postechfastfood.infrastructure.web.api.dtos.PessoaResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
 public class PessoaController {
 
+    private final PessoaService pessoaService;
 
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    @PostMapping("/v1/pessoa")
+    public ResponseEntity<PessoaResponseDto> cadastrarPessoa(@RequestBody PessoaRequestDto pessoaRequestDto) {
+        PessoaResponseDto pessoaResponseDto = pessoaService.cadastrarPessoa(pessoaRequestDto);
+        return ResponseEntity.created(URI.create("/api/v1/pessoa/" + pessoaResponseDto.cdDocPessoa()))
+                .body(pessoaResponseDto);
+    }
+
+    private PessoaRequestDto toRequest(PessoaResponseDto dto) {
+        return new PessoaRequestDto(dto.cdDocPessoa(), dto.nmPessoa(), dto.tpPessoa(), dto.dsEmail());
+    }
+
+    public PessoaResponseDto toResponse(PessoaRequestDto dto) {
+        return new PessoaResponseDto(dto.cdDocPessoa(), dto.nmPessoa(), dto.tpPessoa(), dto.dsEmail());
+    }
 }
