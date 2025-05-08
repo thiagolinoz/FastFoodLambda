9e970d10c539f98ed5c02f459d7a12070d673b58
Alex Carreira <alex@cgtecnologia.com.br>
2025-05-07 21:27:12 -0300

    First Commit

---
+++ src/main/java/br/com/fiap/postechfastfood/PostechFastfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postechfastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechFastfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostechFastfoodApplication.class, args);
+    }
+
+}
