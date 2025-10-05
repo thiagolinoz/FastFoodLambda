2f0f0ae2363d341ecad315e72e61dd2b01de817c
thiagolinoz <thiagolinoz@gmail.com>
2025-10-05 08:54:18 -0300

    remove arquivos duplicados de outros repos

--- infra/gateway.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = data.terraform_remote_state.eks_vpc.outputs.vpc_id
-  tags   = var.tags
-}
--- infra/subnet-group.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_db_subnet_group" "aurora_subnet_group" {
-  name       = "aurora-subnet-group"
-  subnet_ids = [data.terraform_remote_state.eks_vpc.outputs.public_subnet_ids[0],data.terraform_remote_state.eks_vpc.outputs.public_subnet_ids[1], data.terraform_remote_state.eks_vpc.outputs.public_subnet_ids[2]]
-  tags       = var.tags
-}
