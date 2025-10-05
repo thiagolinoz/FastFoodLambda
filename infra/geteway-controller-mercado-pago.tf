b456f46ba7b1953f577eba7117aab2a0f7066fa8
thiagolinoz <thiagolinoz@gmail.com>
2025-10-05 10:06:22 -0300

    corrige resource do mercado-pago

--- infra/geteway-controller-mercado-pago.tf
+++ infra/geteway-controller-mercado-pago.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_resource" "webhook" {
   rest_api_id = aws_api_gateway_rest_api.rest_api_fastfood.id
-  parent_id   = aws_api_gateway_resource.v1_fastfood.id
+  parent_id   = aws_api_gateway_rest_api.rest_api_fastfood.root_resource_id
   path_part   = "webhook"
 }
 
