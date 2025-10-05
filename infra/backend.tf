d653724fef7f3012a6e52243b16dc353b75e5e42
thiagolinoz <thiagolinoz@gmail.com>
2025-10-05 08:48:30 -0300

    corrige nome bucket lambda

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-fiap-fastfood-backend-1"
+    bucket = "postech-fiap-fastfood-backend-lambda-lino"
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
